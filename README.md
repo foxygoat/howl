9f18df228acd68757b5cc0c2147294c77a258a4d
Julia Ogris <julia.ogris@gmail.com>
2021-06-16 17:34:11 +1000

    Initialise howl GitHub action
    Initialise howl GitHub action with the core script `slack-notify.sh`
    and README.md documenting the usage.
    
    Signed-off-by: Julia Ogris <julia.ogris@gmail.com>

---
+++ README.md
@@ -0,0 +1,77 @@
+# Howl
+
+Because somebody should yell at you when your `master` build breaks.
+
+Howl is a GitHub Action. It writes a custom Slack message with an
+optional `@here` or `@project-owners` to your preferred Slack channel.
+
+<div style="text-align:center;width:100%">
+	<img src="howler.jpeg" alt="Howler Monkey"/>
+	<br/>
+	<sub>Howler Monkey -
+		<a href="https://commons.wikimedia.org/wiki/File:DSC09108_-_Guyanan_Red_Howler_Monkey_(36384553204).jpg">
+			Wikimedia, CC BY-SA 2.0
+		</a>
+	</sub>
+</div>
+
+## Set up Slack token
+
+Set up a repository or organisation secrets with your [Incoming Slack
+Webhook] token. Use, for instance, `SLACK_TOKEN`. The content should
+look similar to
+
+	T01A*******/B0259*****/GcXTiEux*******************
+
+Taken from the Slack provided Incoming Webhook URL:
+
+	https://hooks.slack.com/services/T01A*******/B0259*****/GcXTiEux*******************
+
+[Incoming Slack Webhook]: https://slack.com/intl/en-au/help/articles/115005265063-Incoming-webhooks-for-Slack
+
+## Use with GitHub Action Workflow
+
+```yaml
+name: ci
+on:
+  push:
+    branches: [ master ]
+  pull_request:
+
+jobs:
+  ci:
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@v2
+      run: make
+    - if: ${{ failure() && github.event_name == 'push' }} # only run for failed build on master
+      uses: foxygoat/howl
+      env:
+        SLACK_TOKEN: ${{ secrets.SLACK_TOKEN }}
+        #SLACK_TEXT: <!here|here>   # optional; text or @-mention project owners by slack member ID, e.g. <@U0LAN0Z89>
+        #CHANNEL: D01J5K3RLQJ       # optional; use if different from slack webhook setup, take from channel URL
+```
+
+## Use with external CI system
+
+```yaml
+name: slack-notify
+on:
+  # choose one of status, check_run, check_suite
+  status:
+  check_run:
+    types: [completed]
+  check_suite:
+    types: [completed]
+jobs:
+  slack-notify:
+    runs-on: ubuntu-latest
+    steps:
+    - if: ${{ contains(github.event.branches.*.name, 'master') && (github.event.state == 'failure' || github.event.state == 'error')}}
+      uses: foxygoat/howl
+      env:
+        SLACK_TOKEN: ${{ secrets.SLACK_TOKEN }}
+        BUILD_URL: ${{ github.event.target_url }}
+        #SLACK_TEXT: <!here|here>   # optional; text or @-mention project owners by slack member ID, e.g. <@U0LAN0Z89>
+        #CHANNEL: D01J5K3RLQJ       # optional; use if different from slack webhook setup, take from channel URL
+```
---
+++ slack-notify.sh
@@ -0,0 +1,85 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [[ -z "${SLACK_TOKEN-}" ]]; then
+    read -r SLACK_TOKEN
+fi
+SLACK_HOOK_URL="https://hooks.slack.com/services/${SLACK_TOKEN}"
+
+: "${GITHUB_SERVER_URL:=https://github.com}"
+: "${GITHUB_API_URL:=https://api.github.com}"
+: "${GITHUB_SHA:=$(git rev-parse HEAD)}"
+: "${PICTURE_BASE_URL:=https://github.com/foxgoat/howl/raw/master}"
+
+if [[ -z "${GITHUB_REPOSITORY-}" ]]; then
+    REPO_URL=$(git remote get-url origin)
+    GITHUB_REPOSITORY="${REPO_URL#${GITHUB_SERVER_URL}/}"
+else
+    REPO_URL="${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}"
+fi
+REPO=${GITHUB_REPOSITORY#*/}
+
+: "${BUILD_URL:=${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions${GITHUB_RUN_ID:+/runs/${GITHUB_RUN_ID}}}"
+: "${GITHUB_REF:=$(git symbolic-ref HEAD)}"
+BRANCH=${GITHUB_REF#refs/heads/}
+BRANCH_URL="${REPO_URL}/tree/${BRANCH}"
+
+# prop extracts property from JSON object.
+# prop '{"name": "Tricia", "age": 42}' age
+# 42
+prop() {
+    jq -r ".$2" <<<"$1"
+}
+
+CURL_HEADERS=(-H "Accept: application/vnd.github.groot-preview+json")
+if [[ -n "${GITHUB_TOKEN-}" ]]; then
+    CURL_HEADERS+=(-H "Authorization: token ${GITHUB_TOKEN}")
+fi
+
+COMMIT_URL="${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/commits/${GITHUB_SHA}"
+pr=$(curl -fsSL "${CURL_HEADERS[@]}" "${COMMIT_URL}/pulls" |
+    jq '.[0] | {url: .html_url, number, title, author: .user.login}' || echo '{}')
+if [[ $(prop "${pr}" number) != null ]]; then
+    PR="PR #$(prop "${pr}" number)"
+    PR_URL=$(prop "${pr}" url)
+    PR_TITLE=$(prop "${pr}" title)
+    PR_AUTHOR=$(prop "${pr}" author)
+else
+    commit=$(curl -fsSL "${CURL_HEADERS[@]}" "${COMMIT_URL}" |
+        jq '{url: .html_url, message: .commit.message, author: .author.login}' || echo '{}')
+    PR="Commit \`${GITHUB_SHA::7}\`"
+    if [[ $(prop "${commit}" url) != null ]]; then
+        PR_URL=$(prop "${commit}" url)
+        PR_TITLE=$(prop "${commit}" message | head -n 1)
+        PR_AUTHOR=$(prop "${commit}" author)
+    else
+        PR_URL="${REPO_URL}"
+        PR_TITLE=$(git show -s --pretty=format:%s)
+        PR_AUTHOR=$(git show -s --pretty=format:%an)
+    fi
+fi
+
+curl -fsSL -d @- "${SLACK_HOOK_URL}" <<EOF
+{
+ "icon_url": "${PICTURE_BASE_URL}/icon.png",
+ ${CHANNEL:+"\"channel\": \"${CHANNEL}\","}
+ "username": "${SLACK_USERNAME:-GitHub}",
+ ${SLACK_TEXT:+"\"text\": \"${SLACK_TEXT}\","}
+ "attachments": [
+      {
+          "fallback": "Build failure on ${BRANCH}",
+          "color": "danger",
+          "fields": [
+            {
+                "value": "\
+*<${BUILD_URL}|Build Failure>* on \`<${BRANCH_URL}|${REPO}:${BRANCH}>\`\n\
+<${PR_URL}|${PR}> - ${PR_TITLE} \`@${PR_AUTHOR}\`"
+            }
+          ],
+          "footer": "<${REPO_URL}|${REPO}>",
+          "footer_icon": "${PICTURE_BASE_URL}/footer.png"
+      }
+  ]
+}
+EOF
